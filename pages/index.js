173c124546c365bb352e00363af97856a634a96e
resatugurulu <resatugur.ulu@gmail.com>
2023-11-18 02:36:41 +0300

    install next app w/ publication example

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Publications } from '@lens-protocol/widgets-react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <Publications />
+    </div>
+  )
+}
